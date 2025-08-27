9038376a0ae49cf585849939194e5d9a3485df2b
shahn <shahn@everspin.co.kr>
2025-08-28 08:55:17 +0900

    Test module seperate

--- Util.py
+++
@@ -1,9 +0,0 @@
-GREEN = "\033[32m"
-RED = "\033[31m"
-RESET = "\033[0m"
-
-def printG(msg):
-    print (f"{GREEN}{msg}{RESET}")
-
-def printR(msg):
-    print (f"{RED}{msg}{RESET}")
--- color_test.py
+++
@@ -1,4 +0,0 @@
-for i in range(1,100):
-    color = "\033["+str(i)+"m"
-    RESET = "\033[0m"
-    print(f"{color}color test: {i}m{RESET}")
