649c612d5ecdf2dde7db1134e6a8b15322cb3cc9
shahn <shahn@everspin.co.kr>
2025-08-29 14:27:08 +0900

    Add Subfinder on shell

--- info_scrap.sh
+++ info_scrap.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-base_url="http://localhost:8000/"  # 대상 base URL로 변경
-domain="everspin.global"  # 대상 Domain로 변경
+base_url="http://minpeter.uk/"  # 대상 base URL로 변경
+domain="minpeter.uk"  # 대상 Domain로 변경
+
+echo "========================================== Subdomain Scan =========================================="
+echo $domain | subfinder -silent | httpx -silent -probe -title -status-code
 
 echo "========================================== Dir/File Scan =========================================="
 python information_scrp/public_dir_file.py "$base_url"
